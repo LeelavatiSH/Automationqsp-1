04667b49d86c8e2541fd937801e53bbbf0fa87de
vikra <vikra@LAPTOP-5JVD2NPO>
2021-09-16 21:17:03 +0530

    my first commit

---
+++ automationqsp/src/qsp/CloseFirefox.java
@@ -0,0 +1,14 @@
+package qsp;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CloseFirefox {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+        FirefoxDriver driver=new FirefoxDriver();
+        Thread.sleep(10000);
+       driver.close();
+	}
+
+}
